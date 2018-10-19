50fcd65cbbbfffd82f5600fefef238f1a397c79f
Yali kwang <yalikwang@gmail.com>
2018-10-19 21:11:44 +0800

    Create a super class for swipetableVC and adding colour to category

--- Todo/Controllers/CategoryViewController.swift
+++ Todo/Controllers/CategoryViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 import RealmSwift
-import SwipeCellKit
+import ChameleonFramework
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
 
@@ -20,6 +20,8 @@ class CategoryViewController: UITableViewController {
         super.viewDidLoad()
         
         loadCategories()
+        
+        tableView.separatorStyle = .none
         
         tableView.rowHeight = 80.0
     }
@@ -32,20 +34,15 @@ class CategoryViewController: UITableViewController {
         
     }
     
-    //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
-    //        cell.delegate = self
-    //        return cell
-    //    }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for:indexPath) as! SwipeTableViewCell
         
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+            
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Category Added Yet"
         
-        cell.delegate = self
+        cell.backgroundColor = UIColor(hexString: categories?[indexPath.row].colour ?? "1D9BF6")
         
         return cell
     }
@@ -95,6 +92,25 @@ class CategoryViewController: UITableViewController {
         
     }
     
+    //Mark: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        
+        //Below the code allows to print out the delete line
+        //super.updateModel(at: indexPath)
+        
+                if let categoryForDeletion = self.categories?[indexPath.row] {
+                    do {
+                        try self.realm.write {
+                            self.realm.delete(categoryForDeletion)
+                        }
+                    } catch {
+                        print("Error deleting category,\(error)")
+                    }
+    
+                }
+    }
+    
     //MARK: - Add New Categories
     
     
@@ -108,6 +124,7 @@ class CategoryViewController: UITableViewController {
         
             let newCategory = Category()
             newCategory.name = textField.text!
+            newCategory.colour = UIColor.randomFlat.hexValue()
             
             self.save(category: newCategory)
             
@@ -124,40 +141,3 @@ class CategoryViewController: UITableViewController {
     }
 }
 
-//Mark: - Swipe Cell Delegate Methods
-
-extension CategoryViewController: SwipeTableViewCellDelegate {
-    
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        
-        guard orientation == .right else { return nil }
-        
-        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            // handle action by updating model with deletion
-         
-            if let categoryForDeletion = self.categories?[indexPath.row] {
-            do {
-            try self.realm.write {
-                self.realm.delete(categoryForDeletion)
-            }
-            } catch {
-                print("Error deleting category,\(error)")
-            }
-            
-        }
-            
-    }
-        
-        // customize the action appearance
-        deleteAction.image = UIImage(named: "delete-icon")
-        
-        return [deleteAction]
-    }
-    
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
-        var options = SwipeOptions()
-        options.expansionStyle = .destructive
-        return options
-    }
-    
-}
--- Todo/Controllers/TodoListViewController.swift
+++ Todo/Controllers/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
 
     let realm = try! Realm()
     
@@ -33,6 +33,7 @@ class TodoListViewController: UITableViewController {
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
+        tableView.rowHeight = 60.0
         
     }
     //MARK: - TAbleView Datasource Methods
@@ -46,8 +47,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-    
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row]{
             
@@ -63,17 +63,13 @@ class TodoListViewController: UITableViewController {
         
         return cell
         
-
-        
         //it is the same as below code
 //        if itemArray[indexPath.row].done == true {
 //            cell.accessoryType = .checkmark
 //        } else {
 //            cell.accessoryType = .none
 //        }
-        
-        
-        return cell
+
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -163,6 +159,18 @@ class TodoListViewController: UITableViewController {
 
         tableView.reloadData()
     }
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = todoItems?[indexPath.row] {
+            do {
+            try realm.write {
+                realm.delete(item)
+            }
+            } catch {
+                print("Error deleting Item, \(error)")
+            }
+    }
+}
     
 }
 
